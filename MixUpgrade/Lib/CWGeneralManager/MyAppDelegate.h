eff6febd3d06b65731e6e342bcbcfddf10a0b77a
Luociwei <luociwei86@126.com>
2021-01-15 01:09:11 +0800

    v0.3

---
+++ MixUpgrade/Lib/CWGeneralManager/MyAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MyAppDelegate.h
+//  MixUpgrade
+//
+//  Created by ciwei luo on 2021/1/15.
+//  Copyright © 2021 macdev. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyAppDelegate : AppDelegate
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MixUpgrade/Lib/CWGeneralManager/MyAppDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MyAppDelegate.m
+//  MixUpgrade
+//
+//  Created by ciwei luo on 2021/1/15.
+//  Copyright © 2021 macdev. All rights reserved.
+//
+
+#import "MyAppDelegate.h"
+
+@implementation MyAppDelegate
+
+@end
